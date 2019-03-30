cd1cbd2d033c48efe85e32171d674b2262edcec9
Maciek12k <Detsoncry@gmail.com>
2019-03-30 16:31:50 +0100

    Initial commit

---
+++ Assets/SSSS.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSSS : MonoBehaviour
+{
+    public Transform tf;
+    
+    void Update()
+    {
+        transform.rotation = tf.rotation;
+    }
+}
